d8ab1a08274851f4be7259f2de0df28889926f3d
kUcLis <ulis89@gmail.com>
2023-01-25 10:02:21 +0100

    Initial and last commit for this program, practicing GIT

---
+++ Finding Divisible by 3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Finding_Divisible_by_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for(int i = 0; i < 21; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    sum += i;
+                } 
+            }
+            Console.WriteLine($"Sum of all integers divisible by 3 is {sum}");
+            Console.ReadLine();
+        }
+    }
+}
